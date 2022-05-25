a398c4a4c8f4d777c87225a1fe873b08e39dee34
Ewelina Głębocka <eglebocka95@gmail.com>
2022-05-25 15:25:52 +0200

    New class api added

---
+++ OnlineLibrary/OnlineLibrary.BLL/Database/ApiFreeLecture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl.Http;
+using OnlineLibrary.BLL.Enums;
+using OnlineLibrary.BLL.Models;
+
+namespace OnlineLibrary.BLL.Database
+{
+    // GET
+    public class ApiFreeLecture
+    {
+        public static async Task GetBooksApi()
+        {
+            var result = await "https://wolnelektury.pl/api/books/"
+                .GetAsync()
+                .ReceiveJson<List<Book>>();
+        }
+
+        public static async Task GetAuthorApi()
+        {
+            var result = await "https://wolnelektury.pl/api/authors/"
+                .GetAsync()
+                .ReceiveJson<List<Author>>();
+        }
+
+        public static async Task GetBookTypeApi()
+        {
+            var result = await "https://wolnelektury.pl/api/kinds/"
+                .GetAsync()
+                .ReceiveJson<List<BookType>>();
+        }
+
+    }
+}
