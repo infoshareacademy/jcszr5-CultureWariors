9a94d7610e4a1393df14e532e21876c7536302a1
Bartosz Sadowski <bshadowsky@gmail.com>
2022-06-22 21:51:44 +0200

    Added Cart Controller, need to add other options to Repository

---
+++ OnlineLibrary/OnlineLibraryASP/Controllers/CartController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineLibrary.BLL.Repositories;
+using OnlineLibraryASP.ViewModels;
+using System.Security.Claims;
+
+namespace OnlineLibraryASP.Areas.Reader
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IShoppingCartRepository _repository;
+        public ShoppingCartViewModel ShoppingCartViewModel { get; set; }
+        public CartController(IShoppingCartRepository repository)
+        {
+            _repository = repository;
+        }
+        // GET: CartController
+        public ActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            ShoppingCartViewModel = new ShoppingCartViewModel()
+            {
+                CartList = _repository.GetAll().Where(u=>u.ApplicationUserId == claim.Value).ToList(),
+            };
+
+            return View(ShoppingCartViewModel);
+        }
+
+        // GET: CartController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CartController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CartController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CartController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CartController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CartController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CartController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ OnlineLibrary/OnlineLibraryASP/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,9 @@
+﻿using OnlineLibrary.BLL.Models;
+
+namespace OnlineLibraryASP.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public IEnumerable<ShoppingCart> CartList { get; set; }
+    }
+}
